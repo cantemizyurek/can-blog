e8e20f390035eb2693ae949a02f750369e2c27c6
Can Temizyurek <can@temizyurek.com>
2023-11-28 21:51:20 -0500

    add about page

---
+++ app/(main)/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-4 mx-auto w-64 md:w-96 lg:w-[560px]">
+      <h1 className="text-4xl font-bold">About</h1>
+      <p>Hi my name is Can Temizyurek. I'm a software developer.</p>
+    </main>
+  )
+}
