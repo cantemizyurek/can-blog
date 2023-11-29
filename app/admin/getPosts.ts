cf80f18a5cd4a1023d1e87c5529be0ba2b71b1f6
Can Temizyurek <can@temizyurek.com>
2023-11-29 11:16:20 -0500

    sperate posts

---
+++ app/admin/getPosts.ts
@@ -0,0 +1,17 @@
+import prisma from '../utils/prismaSingleton'
+
+export default async function getPosts() {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      createdAt: true,
+      published: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return posts
+}
