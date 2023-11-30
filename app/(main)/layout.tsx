6b5dcdace24a5c09d8100f34c133f74080c719b3
Can Temizyurek <can@temizyurek.com>
2023-11-30 15:10:48 -0500

    change color scheme

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-900 text-rose-100 flex flex-col min-h-screen">
+      <body className="bg-zinc-900 text-pink-100 flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-900 text-rose-100 flex flex-col min-h-screen">
+      <body className="bg-zinc-900 text-pink-100 flex flex-col">
         {children}
       </body>
     </html>
