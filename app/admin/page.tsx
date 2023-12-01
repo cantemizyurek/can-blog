1b48ee32ac3d01d8282b430fac3682ffdd4059b2
Can Temizyurek <can@temizyurek.com>
2023-11-30 21:35:53 -0500

    add go back to blog to admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -9,12 +9,20 @@ export default async function Page() {
     <main className="p-4">
       <div className="flex justify-between items-center">
         <h1 className="text-5xl">Posts</h1>
-        <Link
-          href="/admin/posts/new"
-          className="bg-rose-100 text-zinc-900 px-4 py-2 rounded-md hover:bg-rose-200 font-semibold"
-        >
-          New Post
-        </Link>
+        <div className="flex items-center space-x-2">
+          <Link
+            href="/"
+            className="bg-rose-100 text-zinc-900 px-4 py-2 rounded-md hover:bg-rose-200 font-semibold"
+          >
+            Blog
+          </Link>
+          <Link
+            href="/admin/posts/new"
+            className="bg-rose-100 text-zinc-900 px-4 py-2 rounded-md hover:bg-rose-200 font-semibold"
+          >
+            New Post
+          </Link>
+        </div>
       </div>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-4">
         {posts.map(post => (
